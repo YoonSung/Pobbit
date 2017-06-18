b983f5b72297863566ca6ce4540e2efb982858cb
trizdreaming <indie@nhnnext.org>
2017-06-18 13:55:33 +0900

    java crawler(error version) update

--- src/test/java/client/datagov/OpenDataClientTest.java
+++ src/test/java/client/datagov/OpenDataClientTest.java
@@ -51,4 +51,49 @@ public class OpenDataClientTest {
 		Element element = Jsoup.connect("http://watch.peoplepower21.org/?mid=Member&member_seq=892").header("Accept-encoding", "").get().body();
 		System.out.println(element.toString());
 	}
+
+	@Test
+	public void test4() throws IOException {
+		/** 에러났던 java 크롤러
+		 * 일단 의견은 HttpPost를 사용하기 때문에 서버에서 편집인 줄 알고 id/pass를 물어보는 곳으로 이동하는 걸로..
+		 * 하지만 GET으로 바꾸면 코드가 에러라 수정 중
+		 // 2. 가져올 HTTP 주소 세팅
+		 HttpGet http = new HttpGet("http://finance.naver.com/item/coinfo.nhn?code=045510&target=finsum_more");
+
+		 // 3. 가져오기를 실행할 클라이언트 객체 생성
+		 HttpClient httpClient = HttpClientBuilder.create().build();
+
+		 // 4. 실행 및 실행 데이터를 Response 객체에 담음
+		 HttpResponse response = httpClient.execute(http);
+
+		 // 5. Response 받은 데이터 중, DOM 데이터를 가져와 Entity에 담음
+		 HttpEntity entity = response.getEntity();
+
+		 // 6. Charset을 알아내기 위해 DOM의 컨텐트 타입을 가져와 담고 Charset을 가져옴
+		 ContentType contentType = ContentType.getOrDefault(entity);
+		 Charset charset = contentType.getCharset();
+
+		 // 7. DOM 데이터를 한 줄씩 읽기 위해 Reader에 담음 (InputStream / Buffered 중 선택은 개인취향)
+		 BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent(), charset));
+
+		 // 8. 가져온 DOM 데이터를 담기위한 그릇
+		 StringBuffer sb = new StringBuffer();
+
+		 // 9. DOM 데이터 가져오기
+		 String line = "";
+		 while((line=br.readLine()) != null){
+
+		 sb.append(line+"\n");
+		 }
+
+		 // 10. 가져온 아름다운 DOM을 보자
+		 System.out.println(sb.toString());
+
+		 // 11. Jsoup으로 파싱해보자.
+		 Document doc = Jsoup.parse(sb.toString());
+
+		 // 참고 - Jsoup에서 제공하는 Connect 처리
+		 Document doc2 = Jsoup.connect("http://finance.naver.com/item/coinfo.nhn?code=045510&target=finsum_more").get();
+		 */
+	}
 }
